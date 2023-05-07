2b25c4ac37f9392b14ba5ed283bf6702842a0954
GroupWeb21 <80159815+ThanhNghia-JV4@users.noreply.github.com>
2023-05-07 16:32:02 +0700

    update

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -11,28 +11,39 @@ const Footer = () => <footer className="page-footer font-small blue pt-4">
             <hr className="clearfix w-100 d-md-none pb-0"/>
 
             <div className="col-md-3 mb-md-0 mb-3">
-                <h5 className="text-uppercase">Links</h5>
+                <h5 className="text-uppercase">Support Switchboard</h5>
                 <ul className="list-unstyled">
-                    <li><a href="#!">Link 1</a></li>
-                    <li><a href="#!">Link 2</a></li>
-                    <li><a href="#!">Link 3</a></li>
-                    <li><a href="#!">Link 4</a></li>
+                    <li>Nghia: 0867469413</li>
+                    <li>Thuan: 0704797165</li>
+                    
+                    {/* <li><a href="#!">Link 3</a></li>
+                    <li><a href="#!">Link 4</a></li> */}
                 </ul>
             </div>
 
             <div className="col-md-3 mb-md-0 mb-3">
-                <h5 className="text-uppercase">Links</h5>
                 <ul className="list-unstyled">
-                    <li><a href="#!">Link 1</a></li>
-                    <li><a href="#!">Link 2</a></li>
-                    <li><a href="#!">Link 3</a></li>
-                    <li><a href="#!">Link 4</a></li>
+                    <li className="py-1">
+                        <a href="https://www.youtube.com/@sangtaodientu" className="align-items-center p-0 text-dark">
+                            <img src="https://cdn.jsdelivr.net/npm/simple-icons@3.13.0/icons/youtube.svg" width="30px"/>
+                        </a>
+                    </li>
+                    <li className="py-3">
+                        <a href="https://www.facebook.com/caonhuthuan1" className="align-items-center p-0 text-dark">
+                            <img src="https://cdn.jsdelivr.net/npm/simple-icons@3.13.0/icons/facebook.svg" width="30px"/>
+                        </a>
+                    </li>
+                    {/* <li>
+                        <a href="https://www.youtube.com/S%C3%A1ngT%E1%BA%A1o%C4%90i%E1%BB%87nT%E1%BB%AD1" className="align-items-center p-0 text-dark">
+                            <img src="https://cdn.jsdelivr.net/npm/simple-icons@3.13.0/icons/youtube.svg" width="30px"/>
+                        </a>
+                    </li> */}
                 </ul>
             </div>
         </div>
     </div>
 
-    <div className="footer-copyright text-center py-3">© 2020 Copyright: Thanh Nghia</div>
+    <div className="footer-copyright text-center py-3">© 2023 Copyright: Thanh Nghia</div>
 
 </footer>
 
